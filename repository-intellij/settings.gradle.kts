fd4b0714e23fe688299e818590d323d18a1c8464
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2026-01-13 15:58:38 +0100

    IDE Plugin Wizard packs - Move IntelliJ Platform packs to a dedicated repository folder

---
+++ repository-intellij/settings.gradle.kts
@@ -0,0 +1,46 @@
+@file:Suppress("UnstableApiUsage")
+enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal()
+        maven("https://packages.jetbrains.team/maven/p/kastle/maven")
+    }
+}
+
+plugins {
+    id("org.jetbrains.kastle") version "1.0.0-SNAPSHOT"
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        mavenLocal()
+        maven("https://packages.jetbrains.team/maven/p/kastle/maven")
+    }
+
+    versionCatalogs {
+        create("libs").apply {
+            from(files("../gradle/libs.versions.toml"))
+        }
+        create("ktor").apply {
+            from("io.ktor:ktor-version-catalog:3.3.1")
+        }
+    }
+}
