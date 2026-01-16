96af355a722b5b57eeb8ffe9100637a6020a4e0f
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2026-01-16 09:18:49 +0100

    IDE Plugin Wizard packs - Adjust packs to the current IDE Plugin wizard

--- repository-intellij/org.jetbrains.intellij.platform/plugin/settings.gradle.kts
+++ repository-intellij/org.jetbrains.intellij.platform/plugin/settings.gradle.kts
@@ -1,6 +1,2 @@
 val pluginName: String by _properties
-rootProject.name = pluginName
-
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
+rootProject.name = pluginName  // TODO: or artifact name?
