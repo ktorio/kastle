7e4eb54da8cb7a49bb46e09ee6ae2d0791bff831
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2026-03-04 16:22:35 +0100

    Make header consistent with JetBrains Marketplace

--- kastle-server/src/jvmMain/kotlin/ui/wizard/WizardHeader.kt
+++ kastle-server/src/jvmMain/kotlin/ui/wizard/WizardHeader.kt
@@ -7,37 +7,39 @@ import kotlinx.html.*
  */
 fun FlowContent.wizardHeader() {
     header("wizard-header") {
-        div("wizard-header-left") {
-            a(href = "/wizard", classes = "wizard-header-logo") {
-                img(
-                    src = "https://plugins.jetbrains.com/docs/intellij/images/intellij-platform-icon.svg",
-                    alt = "IntelliJ Platform"
-                )
-                h1("wizard-header-title") {
-                    +"Plugin Generator"
+        div("wizard-header-content") {
+            div("wizard-header-left") {
+                a(href = "/wizard", classes = "wizard-header-logo") {
+                    img(
+                        src = "https://plugins.jetbrains.com/docs/intellij/images/intellij-platform-icon.svg",
+                        alt = "IntelliJ Platform"
+                    )
+                    h1("wizard-header-title") {
+                        +"IntelliJ Platform Plugin Generator"
+                    }
                 }
             }
-        }
-        nav("wizard-header-nav") {
-            a(href = "https://plugins.jetbrains.com/") {
-                target = "_blank"
-                +"Plugins"
-            }
-            a(href = "https://plugins.jetbrains.com/organizations") {
-                target = "_blank"
-                +"Teams"
-            }
-            a(href = "https://plugins.jetbrains.com/docs/intellij/welcome.html") {
-                target = "_blank"
-                +"For Authors"
-            }
-            a(href = "https://plugins.jetbrains.com/docs/marketplace/about-marketplace.html") {
-                target = "_blank"
-                +"Knowledge Base"
-            }
-            a(href = "https://blog.jetbrains.com/platform/") {
-                target = "_blank"
-                +"Blog"
+            nav("wizard-header-nav") {
+                a(href = "https://plugins.jetbrains.com/") {
+                    target = "_blank"
+                    +"Plugins"
+                }
+                a(href = "https://plugins.jetbrains.com/organizations") {
+                    target = "_blank"
+                    +"Teams"
+                }
+                a(href = "https://plugins.jetbrains.com/docs/intellij/welcome.html") {
+                    target = "_blank"
+                    +"For Authors"
+                }
+                a(href = "https://plugins.jetbrains.com/docs/marketplace/about-marketplace.html") {
+                    target = "_blank"
+                    +"Knowledge Base"
+                }
+                a(href = "https://blog.jetbrains.com/platform/") {
+                    target = "_blank"
+                    +"Blog"
+                }
             }
         }
     }
