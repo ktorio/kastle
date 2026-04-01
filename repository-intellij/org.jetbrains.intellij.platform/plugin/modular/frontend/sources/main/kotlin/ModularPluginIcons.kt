84da79a3b0b6464a20dea71dc86243ddfe51ab39
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2026-04-01 15:11:05 +0200

    Define module per actual Gradle module

---
+++ repository-intellij/org.jetbrains.intellij.platform/plugin/modular/frontend/sources/main/kotlin/ModularPluginIcons.kt
@@ -0,0 +1,9 @@
+package kastle
+
+import com.intellij.ui.IconManager
+
+@Suppress("unused")
+object ModularPluginIcons {
+    @JvmField
+    val ToolWindow = IconManager.getInstance().getIcon("/icons/toolWindow.svg", javaClass.getClassLoader())
+}
---
+++ repository-intellij/org.jetbrains.intellij.platform/plugin/modular/shared/sources/main/kotlin/Searchable.kt
@@ -0,0 +1,8 @@
+package kastle
+
+/**
+ * Represents an entity that can be filtered by a search query.
+ */
+interface Searchable {
+    fun matches(query: String): Boolean
+}
