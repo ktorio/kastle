6220c789cf65442ac4137f04acc586325cfe83df
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2026-04-22 15:55:27 +0200

    Do not render @Nls for types when the Kotlin plugin is enabled
    Currently, the Kotlin plugin brings its own version of JetBrains Annotations library which clashes with the one from the platform.
    Kotlin's version is taken effectively, and it doesn't support @Nls on type usages.
    This should be reverted when the Kotlin doesn't bundle JetBrains Annotations JAR.

---
+++ testSnapshots/intellij-plugin-kotlin-with-samples/build.gradle.kts
@@ -0,0 +1,66 @@
+import org.jetbrains.changelog.Changelog
+import org.jetbrains.changelog.markdownToHTML
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.changelog")
+    id("org.jetbrains.intellij.platform")
+}
+
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
+dependencies {
+    testImplementation(libs.junit)
+
+    // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
+    intellijPlatform {
+        intellijIdea("1.0.0")
+        testFramework(TestFrameworkType.Platform)
+
+        // Add plugin dependencies for compilation here:
+        bundledPlugin("org.jetbrains.kotlin")
+    }
+}
+
+// Configure IntelliJ Platform Gradle Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
+intellijPlatform {
+    pluginConfiguration {
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
+        description = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
+            val start = "<!-- Plugin description -->"
+            val end = "<!-- Plugin description end -->"
+
+            with(it.lines()) {
+                if (!containsAll(listOf(start, end))) {
+                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+                }
+                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+            }
+        }
+
+        val changelog = project.changelog // local variable for configuration cache compatibility
+        // Get the latest available change notes from the changelog file
+        changeNotes = version.map { pluginVersion ->
+            with(changelog) {
+                renderItem(
+                    (getOrNull(pluginVersion) ?: getUnreleased())
+                        .withHeader(false)
+                        .withEmptySections(false),
+                    Changelog.OutputType.HTML,
+                )
+            }
+        }
+    }
+}
+
+// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
+changelog {
+    groups.empty()
+    versionPrefix = ""
+}
+
+tasks {
+    publishPlugin {
+        dependsOn(patchChangelog)
+    }
+}
