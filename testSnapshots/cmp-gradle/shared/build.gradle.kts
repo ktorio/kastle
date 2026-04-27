3e4470d86235009d515b58fedf0239569aac6809
Maciej Procyk <maciej.procyk@jetbrains.com>
2026-04-27 13:42:28 +0200

    [KMT-2279] compose-multiplatform: use implementation scope for shared compose deps

--- testSnapshots/cmp-gradle/shared/build.gradle.kts
+++ testSnapshots/cmp-gradle/shared/build.gradle.kts
@@ -43,14 +43,14 @@ kotlin {
 
     sourceSets {
         commonMain.dependencies {
-            api(libs.androidx.lifecycle.runtimeCompose)
-            api(libs.androidx.lifecycle.viewmodelCompose)
-            api(libs.compose.components.resources)
-            api(libs.compose.foundation)
-            api(libs.compose.material3)
-            api(libs.compose.runtime)
-            api(libs.compose.ui)
-            api(libs.compose.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.androidx.lifecycle.viewmodelCompose)
+            implementation(libs.compose.components.resources)
+            implementation(libs.compose.foundation)
+            implementation(libs.compose.material3)
+            implementation(libs.compose.runtime)
+            implementation(libs.compose.ui)
+            implementation(libs.compose.uiToolingPreview)
         }
 
         commonTest.dependencies {
