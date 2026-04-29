d4f6df3cee9c8eb30cf77b07fa7319f0f3d61d90
Maciej Procyk <maciej.procyk@jetbrains.com>
2026-04-29 09:54:26 +0200

    [KMT-2279] gradle: drop subprojects group/version and add plugins comment

--- testTemplates/repository/org.gradle/gradle/root.build.gradle.kts
+++ testTemplates/repository/org.gradle/gradle/root.build.gradle.kts
@@ -1,12 +1,9 @@
 val versionCatalogEnabled: Boolean by _properties
 
 plugins {
+    // this is necessary to avoid the plugins to be loaded multiple times
+    // in each subproject's classloader
     for (plugin in _project.gradle.plugins) {
         alias(_unsafe("${plugin.name}")) apply false
     }
-}
-
-subprojects {
-    group = _project.group
-    version = "1.0.0-SNAPSHOT"
 }
