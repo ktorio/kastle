7e38257b2f2d60d4557d14ecf54402864771a00d
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2026-05-12 08:48:29 +0200

    Delete generateRandomHash

--- kastle-core/src/commonMain/kotlin/utils/Random.kt
+++ kastle-core/src/commonMain/kotlin/utils/Random.kt
@@ -6,17 +6,3 @@ fun Random.nextStringId(length: Int = 12): String =
     ByteArray(length).also(this::nextBytes).joinToString("") {
         it.toUByte().toString(16).padStart(2, '0')
     }
-
-/**
- * Generates a random 32-byte hash encoded as a hexadecimal string.
- *
- * @return A randomly generated hash string of 64 hexadecimal characters,
- *         created by encoding a 32-byte random sequence into hexadecimal format.
- */
-fun generateRandomHash(): String {
-    val bytes = Random.nextBytes(32)
-    return bytes.joinToString("") { byte ->
-        val value = byte.toInt() and 0xFF
-        value.toString(16).padStart(2, '0')
-    }
-}
