53a11b5bbc3d76a94c5694a8b53c52673fd64e53
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2026-05-20 10:54:19 +0200

    Enable compiler plugin; update Ktor

--- kastle-server/src/jvmMain/kotlin/Routing.kt
+++ kastle-server/src/jvmMain/kotlin/Routing.kt
@@ -1,18 +1,15 @@
 package org.jetbrains.kastle.server
 
 import io.ktor.server.application.*
-import io.ktor.server.config.property
-import io.ktor.server.config.propertyOrNull
+import io.ktor.server.config.*
 import io.ktor.server.http.content.*
 import io.ktor.server.plugins.di.*
-import io.ktor.server.response.respondText
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.utils.io.*
 import kotlinx.serialization.json.Json
 import org.jetbrains.kastle.PackRepository
 import org.jetbrains.kastle.ProjectGenerator
 
-@OptIn(ExperimentalKtorApi::class)
 fun Application.routing() {
     val repository: PackRepository by dependencies
     val generator: ProjectGenerator by dependencies
