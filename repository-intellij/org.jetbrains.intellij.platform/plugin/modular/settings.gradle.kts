05c1c17b6dff51000d758a808674cb56e734cf51
Karol Lewandowski <karol.lewandowski@jetbrains.com>
2026-06-11 11:58:37 +0200

    Update IJPG plugin version from 2.14.0 to 2.16.0

--- repository-intellij/org.jetbrains.intellij.platform/plugin/modular/settings.gradle.kts
+++ repository-intellij/org.jetbrains.intellij.platform/plugin/modular/settings.gradle.kts
@@ -20,7 +20,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.jetbrains.intellij.platform.settings") version "2.14.0"
+    id("org.jetbrains.intellij.platform.settings") version "2.16.0"
 }
 
 val pluginModuleName: String by _properties
